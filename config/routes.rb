3c1e247560d90730afe8a8f003897ea6a79cc19f
Vu That <92977052+Rittamx031@users.noreply.github.com>
2023-10-25 09:18:22 +0700

    Chapter 6

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  scope "(:locale)", locale: /en|vi/ do
+    resources :users
+    get 'static_pages/home'
+    get 'static_pages/help'
+    get "/signup", to: "users#new"
+    post "/signup", to: "users#create"
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
