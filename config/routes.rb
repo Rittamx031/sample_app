774098a111fa7e0ca6f9d9e763472acfa1a8e6c5
Vu That <92977052+Rittamx031@users.noreply.github.com>
2023-10-30 09:06:24 +0700

    chapter 8

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   scope "(:locale)", locale: /en|vi/ do
     resources :users
     resources :session
