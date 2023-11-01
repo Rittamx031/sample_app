bd32d8649ba7ee37c7687beb33a35af8f3e9f6d6
Vu That <92977052+Rittamx031@users.noreply.github.com>
2023-11-01 13:19:07 +0700

    chapter 8

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,20 @@
 class User < ApplicationRecord
   scope :sorted_by_name, ->{order(:name)}
+
   validates :name, presence: true, length: {maximum: Settings.max_name_length}
   validates :email, presence: true,
     length: {maximum: Settings.max_email_length},
     format: {with: Regexp.new(Settings.email_regex)},
     uniqueness: {case_sensitive: Settings.case_sensitive_email}
+
   has_secure_password
+
+  def self.digest string
+    cost = if ActiveModel::SecurePassword.min_cost
+             BCrypt::Engine::MIN_COST
+           else
+             BCrypt::Engine.cost
+           end
+    BCrypt::Password.create string, cost:
+  end
 end
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,8 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
+pin "scheduler", to: "https://ga.jspm.io/npm:scheduler@0.23.0/index.js"
+pin_all_from "app/javascript/custom", under: "custom"
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
